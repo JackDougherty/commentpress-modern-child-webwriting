a9167eb7f09352da0f91fc2c3fa7e43649e9ce54
Jack Dougherty <jack.dougherty@trincoll.edu>
2013-09-14 09:37:37 -0400

    revised evaluation questions in General comments

--- comments-general.php
+++ comments-general.php
@@ -32,11 +32,11 @@ get_header(); ?>
 <div class="post general_comments">
 
 <h2 class="post_title"><?php _e( 'General comments on the book', 'commentpress-core' ); ?></h2>
-<p>To evaluate the current work as a whole, readers may wish to respond to these review questions. (To comment on a specific essay, please go to that page.)
+<p>To evaluate the work as a whole, readers may wish to respond to these review questions. (To comment on a specific essay, please go to that page.)
 <ol>
 <li>What is the purpose of the manuscript, and how well does it accomplish this goal in its current form?</li>
 <li>Who do you envision as the intended audience of the manuscript, and does it address their interests and needs?</li>
-<li>Is the manuscript based on sound scholarship?</li>
+<li>Is the manuscript based on pedagogical experience, insightful observation, and sound scholarship?</li>
 <li>Is the overall presentation clear? Is the text well written? Does it make effective use of its digital format?</li>
 <li>Could the organization be improved? If so, how?</li>
 <li>Overall, what are the strongest -- and weakest -- features of the work in its current form?</li>
